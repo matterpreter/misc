b1da30f88e9f3c07dccd11e3402d022423f2e525
matterpreter <matt@cubalo.com>
2017-05-28 19:56:31 -0400

    Adding afl-build.sh
    Added afl-build.sh script to aid in building out the test bench VM.
    Will likely need a few tweaks.

--- fuzzing/README.md
+++ fuzzing/README.md
@@ -1,4 +1,6 @@
 # fuzzing
 Random stuff I use for fuzzing. Mostly AFL and Sulley.   
 
-afl-multi.sh - Script that creates 1 master and N slaves for single-system parallelization. Really useful for compute optimized AWS instnaces with tons of cores.
+afl-build.sh - Script to setup my AFL test bench on an Ubuntu 16.04 VM. Will likely need to be tweaked if anyone else will use it.
+
+afl-multi.sh - Script that creates 1 master and N slaves for single-system parallelization. Really useful for compute optimized AWS instances with tons of cores.
---
+++ fuzzing/afl-build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+###
+# Simple script to automatically build my AFL test bench.
+###
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+#Update and install needed applications if they're not already there.
+apt-get update && apt-get -y upgrade
+apt-get install -y wget build-essential clang screen vim python3 python3-setuptools
+
+#Setup the system for AFL
+echo core >/proc/sys/kernel/core_pattern
+mkdir /root/testcases
+mkdir /root/findings
+
+#Download and install AFL
+rm afl-latest.tgz
+wget http://lcamtuf.coredump.cx/afl/releases/afl-latest.tgz
+tar xzvf afl-latest.tgz
+cd afl*
+make && make install
+cd llvm_mode
+LLVM_CONFIG=llvm-config-3.8 make
+cd ..
+make install
+
+#Download and install afl-utils
+cd /opt
+git clone https://github.com/rc0r/afl-utils.git
+cd afl-utils
+python3 setup.py install
+echo "source /usr/lib/python3.5/site-packages/exploitable-1.32_rcor-py3.5.egg/exploitable/exploitable.py" >> ~/.gdbinit
+
+print "[+] AFL Test Bench ready to go!"

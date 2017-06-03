15763a55a0ac74d9e6918bebdc63e6cbe6602bb7
matterpreter <matt@cubalo.com>
2017-06-03 17:35:23 -0400

    Added PEDA and redirected output to a log file

--- fuzzing/afl-build.sh
+++ fuzzing/afl-build.sh
@@ -9,30 +9,42 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 #Update and install needed applications if they're not already there.
-apt-get update && apt-get -y upgrade
-apt-get install -y wget build-essential clang screen vim python3 python3-setuptools
+echo "[+] Updating system..."
+apt-get update && apt-get -y upgrade >>build.log 2>&1
+echo "[+] Installing required packages..."
+apt-get install -y wget build-essential clang screen vim python3 python3-setuptools gdb >>build.log 2>&1
 
 #Setup the system for AFL
+echo "[+] Installing latest AFL..."
 echo core >/proc/sys/kernel/core_pattern
-mkdir /root/testcases
-mkdir /root/findings
+mkdir ~/testcases
+mkdir ~/findings
 
 #Download and install AFL
 rm afl-latest.tgz
-wget http://lcamtuf.coredump.cx/afl/releases/afl-latest.tgz
-tar xzvf afl-latest.tgz
+wget http://lcamtuf.coredump.cx/afl/releases/afl-latest.tgz >>build.log 2>&1
+tar xzvf afl-latest.tgz >>build.log 2>&1
 cd afl*
-make && make install
+make && make install >>build.log 2>&1
 cd llvm_mode
-LLVM_CONFIG=llvm-config-3.8 make
+LLVM_CONFIG=llvm-config-3.8 make >>build.log 2>&1
 cd ..
-make install
+make install >>build.log 2>&1
 
 #Download and install afl-utils
+echo "[+] Installing afl-utils..."
 cd /opt
-git clone https://github.com/rc0r/afl-utils.git
+git clone https://github.com/rc0r/afl-utils.git >>build.log 2>&1
 cd afl-utils
-python3 setup.py install
+python3 setup.py install >>build.log 2>&1
 echo "source /usr/lib/python3.5/site-packages/exploitable-1.32_rcor-py3.5.egg/exploitable/exploitable.py" >> ~/.gdbinit
 
+#Download and install PEDA
+echo "[+] Installing PEDA..."
+git clone https://github.com/longld/peda.git ~/peda >>build.log 2>&1
+echo "source ~/peda/peda.py" >> ~/.gdbinit
+
+AFL-GCC="$(which afl-gcc)"
+
 echo "[+] AFL Test Bench ready to go!"
+echo "afl-gcc location: ${AFL-GCC}"
